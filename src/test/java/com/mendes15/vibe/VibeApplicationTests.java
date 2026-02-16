919b37deaa53cedbc6c06e86149355a1c372e48f
Aldair <aldairryan20@gmail.com>
2026-02-16 11:31:37 -0300

    initial commit

---
+++ src/test/java/com/mendes15/vibe/VibeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mendes15.vibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
